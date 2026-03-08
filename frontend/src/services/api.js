187ed5b2bb80c227af905dfc440631a186eea333
Anthony <lopezanth661@gmail.com>
2026-03-07 16:28:34 -0800

    fix: use relative API path when REACT_APP_BACKEND_URL not set
    Allows API calls to work through nginx proxy instead of direct localhost calls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const API_BASE = process.env.REACT_APP_BACKEND_URL 
+  ? `${process.env.REACT_APP_BACKEND_URL}/api` 
+  : "/api";
 
 const client = axios.create({
   baseURL: API_BASE,
